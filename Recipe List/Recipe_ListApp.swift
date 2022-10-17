8158a1f94041f2f1958867bfd780e79dd06b4e3e
sammybarman <sambarta.raybarman@gmail.com>
2022-10-17 21:35:01 +0900

    Initial Commit

---
+++ Recipe List/Recipe_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_ListApp.swift
+//  Recipe List
+//
+//  Created by Sambarta Ray Barman on 17/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
